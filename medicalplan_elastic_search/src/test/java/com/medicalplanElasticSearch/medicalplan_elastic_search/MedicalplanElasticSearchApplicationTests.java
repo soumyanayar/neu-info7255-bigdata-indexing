916231190bc46d9556a6ba5b83ee1a0e2b52971f
Soumya Nayar <nayar.s@northeastern.edu>
2022-11-20 00:06:41 -0800

    Changing port details

---
+++ medicalplan_elastic_search/src/test/java/com/medicalplanElasticSearch/medicalplan_elastic_search/MedicalplanElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalplanElasticSearch.medicalplan_elastic_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalplanElasticSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
